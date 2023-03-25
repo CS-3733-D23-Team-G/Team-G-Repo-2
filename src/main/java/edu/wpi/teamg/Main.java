34aeba8bf2cf585b850795d3a2651f565e3a0a9e
kristine-guan <kristineguan24@gmail.com>
2023-03-25 14:14:58 -0400

    App --> GApp
    changing naming per instructions for Team G

--- src/main/java/edu/wpi/teamg/Main.java
+++ src/main/java/edu/wpi/teamg/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamg;
 public class Main {
 
   public static void main(String[] args) {
-    App.launch(App.class, args);
+    GApp.launch(GApp.class, args);
   }
 
   // shortcut: psvm
