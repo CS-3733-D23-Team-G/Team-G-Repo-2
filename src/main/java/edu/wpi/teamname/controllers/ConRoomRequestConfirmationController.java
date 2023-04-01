cc9a59a3450c70d445d847e769d43ccd99599a45
arsimonini <arsimonini@wpi.edu>
2023-04-01 13:49:12 -0400

    Choice Boxes and Drop Down Fixed
    Figured out how to make drop downs work for moving screens. Since it works, it was put on each page.

--- src/main/java/edu/wpi/teamname/controllers/ConRoomRequestConfirmationController.java
+++ src/main/java/edu/wpi/teamname/controllers/ConRoomRequestConfirmationController.java
@@ -3,33 +3,50 @@ package edu.wpi.teamname.controllers;
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class ConRoomRequestConfirmationController {
 
   @FXML MFXButton backToHomeButton;
-  @FXML MFXComboBox<String> serviceRequestComboBox;
   @FXML MFXButton signagePageButton;
   @FXML MFXButton exitButton;
+  @FXML ChoiceBox<String> serviceRequestChoiceBox;
 
   ObservableList<String> list =
       FXCollections.observableArrayList(
-          "Meal Request Form",
-          "Furniture Request Form",
           "Conference Room Request Form",
           "Flowers Request Form",
+          "Furniture Request Form",
+          "Meal Request Form",
           "Office Supplies Request Form");
 
   @FXML
   public void initialize() {
-    serviceRequestComboBox.setItems(list);
+    serviceRequestChoiceBox.setItems(list);
     signagePageButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_PAGE));
     backToHomeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     exitButton.setOnMouseClicked(event -> exit());
+    serviceRequestChoiceBox.setOnAction(event -> loadServiceRequestForm());
+  }
+
+  public void loadServiceRequestForm() {
+    if (serviceRequestChoiceBox.getValue().equals("Meal Request Form")) {
+      Navigation.navigate(Screen.MEAL_REQUEST);
+    } else if (serviceRequestChoiceBox.getValue().equals("Furniture Request Form")) {
+      Navigation.navigate(Screen.FURNITURE_REQUEST);
+    } else if (serviceRequestChoiceBox.getValue().equals("Conference Room Request Form")) {
+      Navigation.navigate(Screen.ROOM_REQUEST);
+    } else if (serviceRequestChoiceBox.getValue().equals("Flowers Request Form")) {
+      Navigation.navigate(Screen.FLOWERS_REQUEST);
+    } else if (serviceRequestChoiceBox.getValue().equals("Office Supplies Request Form")) {
+      Navigation.navigate(Screen.SUPPLIES_REQUEST);
+    } else {
+      return;
+    }
   }
 
   public void exit() {
