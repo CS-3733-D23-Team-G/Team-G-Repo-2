497e3c76fa6ef3456d1a3292b9562ea2a395e9e5
arsimonini <arsimonini@wpi.edu>
2023-03-31 21:18:21 -0400

    Submission Gets All of Repo 1 and Then Some
    This gets every function UI from Repo 1 and makes the header consistent between all of the pages. It also creates all other service request page.
    
    No errors that are visible, all buttons should have been tested.

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -3,14 +3,46 @@ package edu.wpi.teamname.controllers;
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 public class HomeController {
 
-  @FXML MFXButton navigateButton;
+  @FXML MFXButton signagePageButton;
+  @FXML MFXButton exitButton;
+
+  @FXML MFXButton homeToCon;
+  @FXML MFXButton homeToConConfirm;
+  @FXML MFXButton homeToFurn;
+  @FXML MFXButton homeToFurnConfirm;
+  @FXML MFXButton homeToFlow;
+  @FXML MFXButton homeToFlowConfirm;
+  @FXML MFXButton homeToMeal;
+  @FXML MFXButton homeToMealConfirm;
+  @FXML MFXButton homeToOffSupp;
+  @FXML MFXButton homeToOffSuppConfirm;
 
   @FXML
   public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    signagePageButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_PAGE));
+    exitButton.setOnMouseClicked(event -> exit());
+
+    homeToCon.setOnMouseClicked(event -> Navigation.navigate(Screen.ROOM_REQUEST));
+    homeToConConfirm.setOnMouseClicked(event -> Navigation.navigate(Screen.ROOM_REQUEST_SUBMIT));
+    homeToFurn.setOnMouseClicked(event -> Navigation.navigate(Screen.FURNITURE_REQUEST));
+    homeToFurnConfirm.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.FURNITURE_REQUEST_SUBMIT));
+    homeToFlow.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOWERS_REQUEST));
+    homeToFlowConfirm.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.FLOWERS_REQUEST_SUBMIT));
+    homeToMeal.setOnMouseClicked(event -> Navigation.navigate(Screen.MEAL_REQUEST));
+    homeToMealConfirm.setOnMouseClicked(event -> Navigation.navigate(Screen.MEAL_REQUEST_SUBMIT));
+    homeToOffSupp.setOnMouseClicked(event -> Navigation.navigate(Screen.SUPPLIES_REQUEST));
+    homeToOffSuppConfirm.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.SUPPLIES_REQUEST_SUBMIT));
+  }
+
+  public void exit() {
+    Platform.exit();
   }
 }
--- src/main/java/edu/wpi/teamname/navigation/Screen.java
+++ src/main/java/edu/wpi/teamname/navigation/Screen.java
@@ -3,7 +3,18 @@ package edu.wpi.teamname.navigation;
 public enum Screen {
   ROOT("views/Root.fxml"),
   HOME("views/Home.fxml"),
-  SERVICE_REQUEST("views/ServiceRequest.fxml");
+  MEAL_REQUEST("views/MealRequestPage.fxml"),
+  ROOM_REQUEST("views/ConRoomRequestPage.fxml"),
+  FLOWERS_REQUEST("views/FlowersRequestPage.fxml"),
+  FURNITURE_REQUEST("views/FurnitureRequestPage.fxml"),
+  SUPPLIES_REQUEST("views/OfficeSuppRequestPage.fxml"),
+
+  SIGNAGE_PAGE("views/SignagePage.fxml"),
+  ROOM_REQUEST_SUBMIT("views/ConRoomRequestConfirmationPage.fxml"),
+  MEAL_REQUEST_SUBMIT("views/MealRequestConfirmationPage.fxml"),
+  FLOWERS_REQUEST_SUBMIT("views/FlowersRequestConfirmationPage.fxml"),
+  FURNITURE_REQUEST_SUBMIT("views/FurnitureRequestConfirmationPage.fxml"),
+  SUPPLIES_REQUEST_SUBMIT("views/OfficeSuppRequestConfirmationPage.fxml");
 
   private final String filename;
 
