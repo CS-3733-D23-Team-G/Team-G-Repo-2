44faba83aa7cffb2d0bc29f1c035d76d133194a9
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-04-01 18:51:03 -0400

    Signage - ALGOS IN FRONT END :)

--- src/main/java/edu/wpi/teamname/controllers/SignagePageController.java
+++ src/main/java/edu/wpi/teamname/controllers/SignagePageController.java
@@ -2,7 +2,12 @@ package edu.wpi.teamname.controllers;
 
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
+import edu.wpi.teamname.pathFinding.Edge;
+import edu.wpi.teamname.pathFinding.Graph;
+import edu.wpi.teamname.pathFinding.Node;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.util.Random;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +20,12 @@ public class SignagePageController {
   @FXML ChoiceBox<String> serviceRequestChoiceBox;
   @FXML MFXButton signagePageButton;
   @FXML MFXButton exitButton;
+  @FXML MFXButton pathFindButton;
+
+  @FXML MFXTextField startLoc;
+
+  @FXML MFXTextField endLoc;
+
   ObservableList<String> list =
       FXCollections.observableArrayList(
           "Conference Room Request Form",
@@ -30,6 +41,10 @@ public class SignagePageController {
     backToHomeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     exitButton.setOnMouseClicked(event -> exit());
     serviceRequestChoiceBox.setOnAction(event -> loadServiceRequestForm());
+    pathFindButton.setOnMouseClicked(event -> processAStarAlg());
+
+    startLoc.getText();
+    endLoc.getText();
   }
 
   public void loadServiceRequestForm() {
@@ -46,6 +61,53 @@ public class SignagePageController {
     } else {
       return;
     }
+  }
+
+  public String processAStarAlg() {
+    String path = "Bad Path";
+
+    int startNode = Integer.parseInt(startLoc.getText());
+    int endNode = Integer.parseInt(endLoc.getText());
+
+    Node[] N1 = new Node[10];
+    Random r = new Random(5591);
+    for (int i = 0; i < 10; i++) {
+      N1[i] =
+          new Node(
+              String.valueOf(i),
+              (int) r.nextInt(100) + i,
+              (int) r.nextInt(100) + i,
+              "L1",
+              "fsadfasd",
+              "dsfajd;",
+              "jk;ldsjf",
+              "dsfaj;sldk");
+    }
+    Edge[] E1 = new Edge[10];
+    E1[0] = new Edge("e1", N1[0], N1[1]);
+    E1[1] = new Edge("e2", N1[1], N1[2]);
+    E1[2] = new Edge("e3", N1[1], N1[3]);
+    E1[3] = new Edge("e4", N1[2], N1[4]);
+    E1[4] = new Edge("e5", N1[3], N1[4]);
+    E1[5] = new Edge("e6", N1[4], N1[5]);
+    E1[6] = new Edge("e7", N1[5], N1[6]);
+    E1[7] = new Edge("e8", N1[6], N1[7]);
+    E1[8] = new Edge("e9", N1[5], N1[8]);
+    E1[9] = new Edge("e10", N1[8], N1[9]);
+    Graph G1 = new Graph(N1, E1);
+    int[][] Adj = G1.createWeightedAdj();
+    // new int[10][10];
+
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        System.out.print(Adj[i][j] + ", ");
+      }
+      System.out.println();
+    }
+
+    G1.aStarAlg(G1.createWeightedAdj(), startNode, endNode);
+
+    return path;
   }
 
   public void exit() {
---
+++ src/main/java/edu/wpi/teamname/pathFinding/Node.java
@@ -0,0 +1,44 @@
+package edu.wpi.teamname.pathFinding;
+
+public class Node {
+
+  private String NodeID;
+  private int nodeX;
+  private int nodeY;
+  private String floor;
+  private String building;
+  private String nodeType;
+  private String longName;
+  private String shortName;;
+
+  public Node(
+      String NodeID,
+      int nodeX,
+      int nodeY,
+      String floor,
+      String nodeType,
+      String building,
+      String longName,
+      String shortName) {
+    this.NodeID = NodeID;
+    this.nodeX = nodeX;
+    this.nodeY = nodeY;
+    this.floor = floor;
+    this.nodeType = nodeType;
+    this.building = building;
+    this.longName = longName;
+    this.shortName = shortName;
+  }
+
+  public int getNodeX() {
+    return nodeX;
+  }
+
+  public int getNodeY() {
+    return nodeY;
+  }
+
+  public String getNodeID() {
+    return NodeID;
+  }
+}
