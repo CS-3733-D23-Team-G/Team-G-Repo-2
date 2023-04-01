80b5ec1a5edf97dbce483495a4f180953a4385a8
kristine-guan <kristineguan24@gmail.com>
2023-04-01 13:38:28 -0400

    Meal Form Textfields match DB
    Textfields in the mealform page now match what the backend will look for to create the request objects. the class that stored mealrequest data has been deleted ( will be replaced by database)

--- src/main/java/edu/wpi/teamname/controllers/MealRequestController.java
+++ src/main/java/edu/wpi/teamname/controllers/MealRequestController.java
@@ -2,7 +2,6 @@ package edu.wpi.teamname.controllers;
 
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
-import edu.wpi.teamname.requestforms.MealRequestForm;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.application.Platform;
@@ -15,7 +14,7 @@ public class MealRequestController {
   @FXML MFXButton exitButton;
 
   // TextFields
-  @FXML MFXTextField mealNameData;
+  // @FXML MFXTextField mealNameData;
   @FXML MFXTextField mealEmployeeIDData;
   @FXML MFXTextField mealDeliveryLocationData;
   @FXML MFXTextField mealPersonOrderingForData;
@@ -29,7 +28,7 @@ public class MealRequestController {
     backToHomeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     exitButton.setOnMouseClicked(event -> exit());
 
-    mealNameData.getText();
+    //  mealNameData.getText();
     mealEmployeeIDData.getText();
     mealDeliveryLocationData.getText();
     mealPersonOrderingForData.getText();
@@ -40,51 +39,51 @@ public class MealRequestController {
   public void exit() {
     Platform.exit();
   }
-
-  public MealRequestForm storeMealValues() {
-    String mealName = mealNameData.getText();
-    String mealEmployeeID = mealEmployeeIDData.getText();
-    String mealLocation = mealDeliveryLocationData.getText();
-    String mealOrderer = mealPersonOrderingForData.getText();
-    String mealNotes = mealNotesData.getText();
-    String foodChoice = mealFoodChoice.getText();
-
-    System.out.println(
-        "Answers: "
-            + mealName
-            + " "
-            + mealEmployeeID
-            + " "
-            + mealLocation
-            + " "
-            + mealOrderer
-            + " "
-            + mealNotes
-            + " "
-            + foodChoice
-            + " ");
-
-    MealRequestForm mealForm =
-        new MealRequestForm(
-            mealName, mealEmployeeID, mealLocation, mealOrderer, mealNotes, foodChoice);
-    printMealForms(mealForm);
-
-    return mealForm;
-  }
-
-  public void printMealForms(MealRequestForm form) {
-
-    System.out.println(
-        form.mealName
-            + " "
-            + form.mealEmployeeID
-            + " "
-            + form.mealLocation
-            + " "
-            + form.mealOrderer
-            + " "
-            + form.mealNotes
-            + " "
-            + form.mealFoodChoice);
-  }
+  //
+  //  public MealRequestForm storeMealValues() {
+  //    String mealName = mealNameData.getText();
+  //    String mealEmployeeID = mealEmployeeIDData.getText();
+  //    String mealLocation = mealDeliveryLocationData.getText();
+  //    String mealOrderer = mealPersonOrderingForData.getText();
+  //    String mealNotes = mealNotesData.getText();
+  //    String foodChoice = mealFoodChoice.getText();
+  //
+  //    System.out.println(
+  //        "Answers: "
+  //            + mealName
+  //            + " "
+  //            + mealEmployeeID
+  //            + " "
+  //            + mealLocation
+  //            + " "
+  //            + mealOrderer
+  //            + " "
+  //            + mealNotes
+  //            + " "
+  //            + foodChoice
+  //            + " ");
+  //
+  //    MealRequestForm mealForm =
+  //        new MealRequestForm(
+  //            mealName, mealEmployeeID, mealLocation, mealOrderer, mealNotes, foodChoice);
+  //    printMealForms(mealForm);
+  //
+  //    return mealForm;
+  //  }
+  //
+  //  public void printMealForms(MealRequestForm form) {
+  //
+  //    System.out.println(
+  //        form.mealName
+  //            + " "
+  //            + form.mealEmployeeID
+  //            + " "
+  //            + form.mealLocation
+  //            + " "
+  //            + form.mealOrderer
+  //            + " "
+  //            + form.mealNotes
+  //            + " "
+  //            + form.mealFoodChoice);
+  //  }
 }
