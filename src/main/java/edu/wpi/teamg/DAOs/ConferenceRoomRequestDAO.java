4bcbbb2c210a44821b7ff6046705f24bd60d663a
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-02 21:10:22 -0400

    Finished getAll
    created the getAll for conferenceRoomRequestsDAO

--- src/main/java/edu/wpi/teamg/DAOs/ConferenceRoomRequestDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/ConferenceRoomRequestDAO.java
@@ -3,7 +3,11 @@ package edu.wpi.teamg.DAOs;
 import edu.wpi.teamg.DBConnection;
 import edu.wpi.teamg.ORMClasses.ConferenceRoomRequest;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,10 +17,41 @@ public class ConferenceRoomRequestDAO implements DAO{
     private String SQL_maxID;
     private String SQL_confRoomRequest;
     private String SQL_Request;
+    private HashMap<Integer,ConferenceRoomRequest> conferenceRequestHash =
+            new HashMap<Integer,ConferenceRoomRequest>();
 
     @Override
-    public HashMap<String, ConferenceRoomRequest> getAll() throws SQLException {
-        return null;
+    public HashMap<Integer, ConferenceRoomRequest> getAll() throws SQLException {
+        db.setConnection();
+        PreparedStatement ps;
+        ResultSet rs = null;
+
+        SQL_confRoomRequest = "select * from teamgdb.proto2.request join " +
+                "teamgdb.proto2.conferenceroomrequest on teamgdb.proto2.request.reqid = " +
+                "teamgdb.proto2.conferenceroomrequest.reqid";
+
+        try{
+            ps = db.getConnection().prepareStatement(SQL_confRoomRequest);
+            rs=ps.executeQuery();
+        }catch(SQLException e){
+            System.err.println("SQL Exception");
+        }
+
+        while(rs.next()){
+            ConferenceRoomRequest cReq = new ConferenceRoomRequest();
+            int reqID = rs.getInt("reqid");
+            Date reqDate = rs.getDate("meetingdate");
+            Time reqTime = rs.getTime("meetingtime");
+            String confPurpose = rs.getString("purpose");
+
+            cReq.setPurpose(confPurpose);
+            cReq.setMeeting_time(reqTime);
+            cReq.setMeeting_date(reqDate);
+
+            conferenceRequestHash.put(reqID,cReq);
+        }
+        db.closeConnection();
+        return conferenceRequestHash;
     }
 
     @Override
