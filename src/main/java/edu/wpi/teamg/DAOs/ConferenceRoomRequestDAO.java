72c96648ded1de858bf3702d70533db95ce7f490
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-04-02 21:36:37 -0400

    Created conferenceRoom insert
    created the insert for the conferenceRoom requests, don't have it updating the hash yet, also unsure about how the util.date will translate to the sql.date.

--- src/main/java/edu/wpi/teamg/DAOs/ConferenceRoomRequestDAO.java
+++ src/main/java/edu/wpi/teamg/DAOs/ConferenceRoomRequestDAO.java
@@ -1,5 +1,6 @@
 package edu.wpi.teamg.DAOs;
 
+import com.sun.scenario.effect.impl.prism.PrReflectionPeer;
 import edu.wpi.teamg.DBConnection;
 import edu.wpi.teamg.ORMClasses.ConferenceRoomRequest;
 
@@ -61,8 +62,54 @@ public class ConferenceRoomRequestDAO implements DAO{
 
     @Override
     public void insert(Object obj) throws SQLException {
+        db.setConnection();
+        PreparedStatement ps_getMaxID;
+        PreparedStatement ps_getRoomReq;
+        PreparedStatement ps_Req;
 
+        ResultSet rs = null;
+
+        SQL_maxID = "select reqID from teamgdb.proto2.request order by reqid desc limit 1";
+
+        try{
+            ps_getMaxID = db.getConnection().prepareStatement(SQL_maxID);
+            rs = ps_getMaxID.executeQuery();
+        }catch (SQLException e){
+            System.err.println("SQL Exception");
+            e.printStackTrace();
+        }
+        int maxID = 0;
+        while(rs.next()){
+            maxID = rs.getInt("reqID");
+            maxID++;
+        }
+        SQL_confRoomRequest = "insert  into teamgdb.proto2.conferenceroomrequest(reqid,meetingdate,meetingtime,purpose) values (?,?,?,?)";
+        SQL_Request =
+            "insert into teamgdb.proto2.request(reqid,location,serv_by,status) values (?,?,?,?)";
+
+        try{
+            ps_Req = db.getConnection().prepareStatement(SQL_Request);
+            ps_Req.setInt(1,maxID);
+            ps_Req.setInt(2,((ConferenceRoomRequest)obj).getLocation());
+            ps_Req.setInt(3,((ConferenceRoomRequest)obj).getServ_by());
+            ps_Req.setObject(4,((ConferenceRoomRequest) obj).getStatus(),java.sql.Types.OTHER);
+            ps_Req.executeUpdate();
+
+            ps_getRoomReq = db.getConnection().prepareStatement(SQL_confRoomRequest);
+            ps_getRoomReq.setInt(1,maxID);
+            ps_getRoomReq.setDate(2, (java.sql.Date)((ConferenceRoomRequest)obj).getMeeting_date());
+            ps_getRoomReq.setTime(3,((ConferenceRoomRequest)obj).getMeeting_time());
+            ps_getRoomReq.setString(4,((ConferenceRoomRequest)obj).getPurpose());
+            ps_getRoomReq.executeUpdate();
+        }catch(SQLException e){
+            System.err.println("SQL Exception");
+            e.printStackTrace();
+        }
+
+        db.closeConnection();
     }
+
+
 
     @Override
     public void delete(Object obj) throws SQLException {
