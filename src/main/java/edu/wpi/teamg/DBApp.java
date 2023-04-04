676d401dc39f475fb8f70cee35f570eae13a515d
Mohamed <mbarry02@yahoo.com>
2023-04-03 21:28:36 -0400

    Update DBApp.java

--- src/main/java/edu/wpi/teamg/DBApp.java
+++ src/main/java/edu/wpi/teamg/DBApp.java
@@ -10,22 +10,9 @@ import java.util.List;
 
 public class DBApp {
   public static void main(String[] args) throws SQLException {
-
-    NodeDAO nodedao = new NodeDAO();
-
-    List<Node> allNodes = nodedao.getAll();
-
-    for (Node node : allNodes) {
-      System.out.println("id = " + node.getNodeID());
-    }
-
     MoveDAO moveDAO = new MoveDAO();
     Date date1 = new Date(2002, 9, 8);
 
-    //    List<Move> allMoveID = moveDAO.getAll();
-    //    for (Move move : allMoveID) {
-    //      System.out.println("id = " + move.getNodeID());
-    //    }
 
     Move move = new Move(100, "Hall 5 Level uno", date1);
     moveDAO.update(move);
